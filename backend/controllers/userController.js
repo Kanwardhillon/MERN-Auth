84323f4c988fc7e3b2edbf730e7d29836ed15a41
Kanwarpal Singh Dhillon <dhillonkanwar13@gmail.com>
2023-07-01 04:51:33 -0400

    first commit

---
+++ backend/controllers/userController.js
@@ -0,0 +1,11 @@
+// @desc   Auth user/set token
+// route   POST /api/users/auth
+// @access Public
+
+const authUser = (req, res) =>{
+    res.status(200).json({ message: "Authorized" })
+}
+
+export {
+    authUser
+}
